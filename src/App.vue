8402aee04730374a26749be4f84dc76613867e41
A. A. Sumitro <hello@aasumitro.id>
2022-01-19 10:58:45 +0800

    WIP

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+</script>
+
+<template>
+  <!-- This starter template is using Vue 3 <html setup> SFCs -->
+  <h1 class="text-3xl font-bold underline text-cyan-400">
+    Hello world!
+  </h1>
+</template>
+
+<style>
+/* This starter template is using Vue 3 <style setup> SFCs */
+</style>
